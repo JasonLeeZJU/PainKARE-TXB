bcc1ce8db2beb90e34947b75618fab4fc3656e0f
Anan <gamexueyuhan@gmail.com>
2017-12-22 14:41:06 +0800

    Initial Commit

---
+++ PainKARE-TXBTests/PainKARE_TXBTests.m
@@ -0,0 +1,39 @@
+//
+//  PainKARE_TXBTests.m
+//  PainKARE-TXBTests
+//
+//  Created by Anan on 2017/12/22.
+//  Copyright © 2017年 Anan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PainKARE_TXBTests : XCTestCase
+
+@end
+
+@implementation PainKARE_TXBTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
